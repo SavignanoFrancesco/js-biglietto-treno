16f476ac8bf7939ba0ed4e19682456450da00231
Francesco <francesco.savignano96@gmail.com>
2020-10-14 10:03:05 +0200

    corretto calcolo prezzp_per_chilometri

--- main.js
+++ main.js
@@ -3,27 +3,27 @@ var chilometri = prompt('Inserisci distanza da percorrere in km: ');
 var anni = prompt('Inserisci età: ');
 
 //Inizializzo variabili per calcolare il risultato
-var prezzo_per_chilometro = (chilometri / 0.21);
-var sconto_minorenni = ((prezzo_per_chilometro * 20) / 100);
-var sconto_over65 = ((prezzo_per_chilometro * 40) / 100);
+var prezzo_per_chilometri = (chilometri * 0.21);
+var sconto_minorenni = ((prezzo_per_chilometri * 20) / 100);
+var sconto_over65 = ((prezzo_per_chilometri * 40) / 100);
 var prezzo_finale;
 
 //Sconto per i minorenni
 if (anni < 19){
-    prezzo_finale = (prezzo_per_chilometro - sconto_minorenni);
+    prezzo_finale = (prezzo_per_chilometri - sconto_minorenni);
     document.getElementById('card1').innerHTML = ("Sconto del 20% !!!");
 }
 
 //Sconto per gli over 65
 else if (anni >= 65){
-    prezzo_finale = (prezzo_per_chilometro - sconto_over65);
+    prezzo_finale = (prezzo_per_chilometri - sconto_over65);
     document.getElementById('card1').innerHTML = ("Sconto del 40% !!!");
 }
 
 //Prezzo non scontato
 else{
     document.getElementById('card1').innerHTML = ("Non è previsto alcuno sconto.");
-    prezzo_finale = prezzo_per_chilometro;
+    prezzo_finale = prezzo_per_chilometri;
 }
 
 //Arotonda il risultato fino ai centesimi
