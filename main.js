f42e5d3f1f03448f49500e57ebb3a0c50b75b7ee
Francesco <francesco.savignano96@gmail.com>
2020-10-14 10:13:33 +0200

    aggiunto commento con consegna esercizio e corretto minorenni if(anni < 18)

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//Il programma dovrà chiedere all’utente il
+//numero di chilometri che vuole
+//percorrere e l’età del passeggero.
+//Sulla base di queste informazioni dovrà
+//calcolare il prezzo totale del viaggio.
+//Il prezzo del biglietto è definito in base ai
+//km (0.21 € al km), ma va applicato uno
+//sconto del 20% per i minorenni e del
+//40% per gli over 65.
+
+
+
+
 //Richieste input all'utente
 var chilometri = prompt('Inserisci distanza da percorrere in km: ');
 var anni = prompt('Inserisci età: ');
@@ -9,7 +22,7 @@ var sconto_over65 = ((prezzo_per_chilometri * 40) / 100);
 var prezzo_finale;
 
 //Sconto per i minorenni
-if (anni < 19){
+if (anni < 18){
     prezzo_finale = (prezzo_per_chilometri - sconto_minorenni);
     document.getElementById('card1').innerHTML = ("Sconto del 20% !!!");
 }
